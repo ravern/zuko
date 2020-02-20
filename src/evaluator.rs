a91b97eb7a2374bb73b88026441121c0f5b9b36e
Ravern Koh <ravern@taskade.com>
2020-02-20 22:20:52 +0800

    refactor: convert `evaluator` and `reader` into structs

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -3,11 +3,65 @@ use thiserror::Error;
 use crate::ast::{self, Expr, List};
 
 pub fn eval(expr: Expr) -> Result<Expr, EvalError> {
-  use Expr::*;
+  let mut evalutor = Evaluator::new();
+  evalutor.eval_expr(expr)
+}
 
-  match expr {
-    List(list) => eval_list(*list),
-    expr => Ok(expr),
+pub struct Evaluator {}
+
+impl Evaluator {
+  pub fn new() -> Evaluator {
+    Evaluator {}
+  }
+
+  pub fn eval_expr(&mut self, expr: Expr) -> Result<Expr, EvalError> {
+    use Expr::*;
+
+    match expr {
+      List(list) => self.eval_list(*list),
+      expr => Ok(expr),
+    }
+  }
+
+  pub fn eval_list(&mut self, list: List) -> Result<Expr, EvalError> {
+    use EvalError::*;
+    use Expr::*;
+
+    let ast::List { head, tail } = list;
+
+    let head = self.eval_expr(head)?;
+    let tail = tail
+      .into_iter()
+      .map(|expr| self.eval_expr(expr))
+      .collect::<Result<Vec<Expr>, EvalError>>()?;
+
+    match head {
+      Add => self.eval_add(tail),
+      _ => Err(NotCallable),
+    }
+  }
+
+  pub fn eval_add(&mut self, tail: Vec<Expr>) -> Result<Expr, EvalError> {
+    use Expr::*;
+
+    let tail = tail
+      .into_iter()
+      .map(|expr| self.as_number(expr))
+      .collect::<Result<Vec<f64>, EvalError>>()?;
+
+    let result = tail.into_iter().sum();
+
+    Ok(Number(result))
+  }
+
+  fn as_number(&mut self, expr: Expr) -> Result<f64, EvalError> {
+    use EvalError::*;
+    use Expr::*;
+
+    match expr {
+      Number(number) => Ok(number),
+      _ => Err(InvalidType),
+    }
   }
 }
 
@@ -17,31 +71,4 @@ pub enum EvalError {
   InvalidType,
   #[error("expression not callable")]
   NotCallable,
-}
-
-fn eval_list(list: List) -> Result<Expr, EvalError> {
-  use EvalError::*;
-  use Expr::*;
-
-  let ast::List { head, tail } = list;
-
-  match head {
-    Add => eval_add(tail),
-    _ => Err(NotCallable),
-  }
-}
-
-fn eval_add(tail: Vec<Expr>) -> Result<Expr, EvalError> {
-  use EvalError::*;
-  use Expr::*;
-
-  let arguments = tail
-    .into_iter()
-    .map(|expr| match expr {
-      Number(number) => Ok(number),
-      _ => Err(InvalidType),
-    })
-    .collect::<Result<Vec<f64>, EvalError>>()?;
-
-  Ok(Expr::Number(arguments.into_iter().sum()))
 }
