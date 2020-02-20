f164395d455438289893708861a13f0a2f5d75dc
Ravern Koh <ravern@taskade.com>
2020-02-20 22:20:52 +0800

    refactor `evaluator` and `reader` into structs

--- src/reader.rs
+++ src/reader.rs
@@ -5,8 +5,114 @@ use thiserror::Error;
 use crate::ast::{Expr, List};
 
 pub fn read(source: &str) -> Result<Expr, ReadError> {
-  let mut source = source.chars().peekable();
-  read_value(&mut source)
+  let mut reader = Reader::new(source.chars());
+  reader.read_value()
+}
+
+pub struct Reader<I>
+where
+  I: Iterator<Item = char>,
+{
+  source: Peekable<I>,
+}
+
+impl<I> Reader<I>
+where
+  I: Iterator<Item = char>,
+{
+  pub fn new(source: I) -> Reader<I> {
+    Reader {
+      source: source.peekable(),
+    }
+  }
+
+  pub fn read_value(&mut self) -> Result<Expr, ReadError> {
+    use Expr::*;
+    use ReadError::*;
+
+    self.skip_whitespace();
+
+    let value = match self.source.peek() {
+      Some('(') => List(Box::new(self.read_list()?)),
+      Some(char) if char.is_digit(10) => Number(self.read_number()?),
+      Some('+') => {
+        self.source.next();
+        Add
+      }
+      Some(char) => return Err(UnexpectedChar(*char)),
+      None => return Err(UnexpectedEndOfInput),
+    };
+
+    self.skip_whitespace();
+
+    Ok(value)
+  }
+
+  pub fn read_list(&mut self) -> Result<List, ReadError> {
+    use ReadError::*;
+
+    match self.source.peek() {
+      Some('(') => {}
+      Some(char) => return Err(UnexpectedChar(*char)),
+      None => return Err(UnexpectedEndOfInput),
+    }
+    self.source.next();
+
+    let head = self.read_value()?;
+
+    let mut tail = Vec::new();
+    loop {
+      match self.source.peek() {
+        Some(')') => {
+          self.source.next();
+          break;
+        }
+        None => return Err(UnexpectedEndOfInput),
+        _ => {}
+      }
+
+      tail.push(self.read_value()?);
+    }
+
+    Ok(List { head, tail })
+  }
+
+  pub fn read_number(&mut self) -> Result<f64, ReadError> {
+    use ReadError::*;
+
+    let mut buf = Vec::new();
+    let mut has_decimal = false;
+
+    loop {
+      match self.source.peek() {
+        Some('.') if !has_decimal => has_decimal = true,
+        Some(char) if char.is_digit(10) => {}
+        Some(')') => break,
+        Some(char) if char.is_whitespace() => break,
+        Some(char) => return Err(UnexpectedChar(*char)),
+        None => break,
+      }
+      let char = self.source.next().unwrap();
+
+      buf.push(char);
+    }
+
+    let buf: String = buf.into_iter().collect();
+    let number = buf.parse().unwrap();
+
+    Ok(number)
+  }
+
+  pub fn skip_whitespace(&mut self) {
+    loop {
+      match self.source.peek() {
+        Some(char) if !char.is_whitespace() => break,
+        None => break,
+        _ => {}
+      }
+      self.source.next();
+    }
+  }
 }
 
 #[derive(Debug, Error)]
@@ -15,104 +121,4 @@ pub enum ReadError {
   UnexpectedEndOfInput,
   #[error("unexpected char '{0}'")]
   UnexpectedChar(char),
-}
-
-fn read_value<I>(source: &mut Peekable<I>) -> Result<Expr, ReadError>
-where
-  I: Iterator<Item = char>,
-{
-  use Expr::*;
-  use ReadError::*;
-
-  skip_whitespace(source);
-
-  let value = match source.peek() {
-    Some('(') => List(Box::new(read_list(source)?)),
-    Some(char) if char.is_digit(10) => Number(read_number(source)?),
-    Some('+') => {
-      source.next();
-      Add
-    }
-    Some(char) => return Err(UnexpectedChar(*char)),
-    None => return Err(UnexpectedEndOfInput),
-  };
-
-  skip_whitespace(source);
-
-  Ok(value)
-}
-
-fn read_list<I>(source: &mut Peekable<I>) -> Result<List, ReadError>
-where
-  I: Iterator<Item = char>,
-{
-  use ReadError::*;
-
-  match source.peek() {
-    Some('(') => {}
-    Some(char) => return Err(UnexpectedChar(*char)),
-    None => return Err(UnexpectedEndOfInput),
-  }
-  source.next();
-
-  let head = read_value(source)?;
-
-  let mut tail = Vec::new();
-  loop {
-    match source.peek() {
-      Some(')') => {
-        source.next();
-        break;
-      }
-      None => return Err(UnexpectedEndOfInput),
-      _ => {}
-    }
-
-    tail.push(read_value(source)?);
-  }
-
-  Ok(List { head, tail })
-}
-
-fn read_number<I>(source: &mut Peekable<I>) -> Result<f64, ReadError>
-where
-  I: Iterator<Item = char>,
-{
-  use ReadError::*;
-
-  let mut buf = Vec::new();
-  let mut has_decimal = false;
-
-  loop {
-    match source.peek() {
-      Some('.') if !has_decimal => has_decimal = true,
-      Some(char) if char.is_digit(10) => {}
-      Some(')') => break,
-      Some(char) if char.is_whitespace() => break,
-      Some(char) => return Err(UnexpectedChar(*char)),
-      None => break,
-    }
-    let char = source.next().unwrap();
-
-    buf.push(char);
-  }
-
-  let buf: String = buf.into_iter().collect();
-  let number = buf.parse().unwrap();
-
-  Ok(number)
-}
-
-fn skip_whitespace<I>(source: &mut Peekable<I>)
-where
-  I: Iterator<Item = char>,
-{
-  loop {
-    match source.peek() {
-      Some(char) if !char.is_whitespace() => break,
-      None => break,
-      _ => {}
-    }
-    source.next();
-  }
 }
