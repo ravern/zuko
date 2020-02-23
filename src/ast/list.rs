cc791e2d48437f3eecf811b72d80f8a55db088a4
Ravern Koh <ravernkoh@gmail.com>
2020-02-23 22:23:04 +0800

    refactor: move `List` into own `list` module

--- src/ast/list.rs
+++ src/ast/list.rs
@@ -1,12 +1,7 @@
-use std::collections::BTreeMap;
 use std::iter::Iterator;
 use std::rc::Rc;
 
-#[derive(Clone, Debug)]
-pub enum Expr {
-  List(List),
-  Atom(Atom),
-}
+use super::Expr;
 
 #[derive(Clone, Debug)]
 pub struct List(Option<Rc<Node>>);
@@ -65,50 +60,5 @@ impl Iterator for IntoIter {
     self.0 = tail;
 
     Some(head)
-  }
-}
-
-#[derive(Clone, Debug)]
-pub enum Atom {
-  Number(f64),
-  Symbol(String),
-  String(String),
-  Function(Box<Function>),
-  Native(Native),
-}
-
-#[derive(Clone, Debug)]
-pub struct Function {
-  pub scope: Scope,
-  pub parameters: Vec<String>,
-  pub body: Expr,
-}
-
-#[derive(Clone, Debug)]
-pub enum Native {
-  Begin,
-  Define,
-  Function,
-  Quote,
-}
-
-#[derive(Clone, Debug)]
-pub struct Scope {
-  variables: BTreeMap<String, Expr>,
-}
-
-impl Scope {
-  pub fn new() -> Scope {
-    Scope {
-      variables: BTreeMap::new(),
-    }
-  }
-
-  pub fn get(&self, symbol: &str) -> Option<Expr> {
-    self.variables.get(symbol).cloned()
-  }
-
-  pub fn set(&mut self, symbol: String, expr: Expr) {
-    self.variables.insert(symbol, expr);
   }
 }
