ea3c52e9d2c70d4d8aba7dc7179e27c2cc12f4b5
Ravern Koh <ravernkoh@gmail.com>
2020-12-20 10:08:34 +0800

    fix build errors

--- tests/test.rs
+++ tests/test.rs
@@ -7,17 +7,17 @@ use zuko::{eval, read};
 pub fn fibonacci() {
   let source = fs::read_to_string("tests/fibonacci.zuko").unwrap();
 
-  let read_expr = read::read(&source).unwrap();
+  let read_expr = read::read(source.chars()).unwrap();
   let eval_expr = eval::eval(read_expr).unwrap();
 
-  assert_eq!(eval_expr, Expression::Atom(Atom::Number(6765.0)))
+  assert_eq!(eval_expr, Expression::Atom(Atom::Float(6765.0)))
 }
 
 #[test]
 pub fn fizz_buzz() {
   let source = fs::read_to_string("tests/fizz-buzz.zuko").unwrap();
 
-  let read_expr = read::read(&source).unwrap();
+  let read_expr = read::read(source.chars()).unwrap();
   let eval_expr = eval::eval(read_expr).unwrap();
 
   assert_eq!(eval_expr, Expression::Atom(Atom::String("FizzBuzz".into())))
@@ -27,11 +27,8 @@ pub fn fizz_buzz() {
 pub fn square_root() {
   let source = fs::read_to_string("tests/square-root.zuko").unwrap();
 
-  let read_expr = read::read(&source).unwrap();
+  let read_expr = read::read(source.chars()).unwrap();
   let eval_expr = eval::eval(read_expr).unwrap();
 
-  assert_eq!(
-    eval_expr,
-    Expression::Atom(Atom::Number(2.0000000929222947))
-  )
+  assert_eq!(eval_expr, Expression::Atom(Atom::Float(2.0000000929222947)))
 }
